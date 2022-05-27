5203fd84f2eef697125615e97c6c1fbd940c0309
Quentin Stempert <quentin.stempert@laposte.net>
2022-05-27 13:17:32 +0200

    ReadMe & Spring App

---
+++ suggestion-box-app/suggestion-box-app/src/main/java/qst/suggestion/box/suggestionboxapp/SuggestionBoxAppApplication.java
@@ -0,0 +1,13 @@
+package qst.suggestion.box.suggestionboxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggestionBoxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuggestionBoxAppApplication.class, args);
+	}
+
+}
---
+++ suggestion-box-app/suggestion-box-app/src/test/java/qst/suggestion/box/suggestionboxapp/SuggestionBoxAppApplicationTests.java
@@ -0,0 +1,13 @@
+package qst.suggestion.box.suggestionboxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuggestionBoxAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
